0ea6b86dc2fbb2e059bf7925c73afcc977b631c9
rafee2024 <emailshaik.rafimiya@gmail.com>
2024-09-13 00:11:21 +0530

    feature202 changes by Vivek

--- Application.java
+++ Application.java
@@ -13,6 +13,27 @@ public class HelloWorld {
         int number = reader.nextInt();
 
         // println() prints the following line to the output screen
+
+//featur202 chnges by vivek for the Reports module injuly2024
+class Main {
+
+// create a method
+  public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
+
+  public static void main(String[] args) {
+    int result;
+
+    // call the method
+    // store returned value to result
+    result = square(10);
+
+    System.out.println("Squared value of 10 is: " + result);
+  }
+}
         System.out.println("You entered: " + number);
     }
 }
