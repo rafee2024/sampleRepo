61cd0243d7c25258f8a3a2772addde57c6c66afb
rafee2024 <emailshaik.rafimiya@gmail.com>
2024-09-12 23:55:40 +0530

    feature201 changes in Application.java file by Vinay

--- Application.java
+++ Application.java
@@ -12,6 +12,25 @@ public class HelloWorld {
         // nextInt() reads the next integer from the keyboard
         int number = reader.nextInt();
 
+//feature201 changes made by Vivek july 2024
+ // create a method
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+    // return value
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    
+    int num1 = 25;
+    int num2 = 15;
+
+    // create an object of Main
+    Main obj = new Main();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+
         // println() prints the following line to the output screen
         System.out.println("You entered: " + number);
     }
